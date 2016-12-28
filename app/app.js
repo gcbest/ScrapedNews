a00a10e6e9e65bb05e2f2f792a06a0a708533f26
gcbest <godfrey.c.best@gmail.com>
2016-12-28 17:22:43 -0500

    state added

--- app/app.js
+++ app/app.js
@@ -3,7 +3,16 @@ var ReactDOM = require('react-dom');
 
 var Headlines = require('./components/headline');
 
-ReactDOM.render(
-	<Headlines />,
-	document.getElementById('headers')
+
+
+	ReactDOM.render(
+		// var content = this.getHeadlines();
+		<div className="main">
+			<Headlines />
+		</div>,
+		document.getElementById('headers')
 );
+
+
+
+
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 // // Dependencies:
 
 // // Snatches HTML from URLs
-// // var request = require("request");
+// var request = require("request");
 // // Scrapes our HTML
-// // var cheerio = require("cheerio");
+var cheerio = require("cheerio");
 // var express = require("express");
 // var bodyParser = require("body-parser");
-// var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 // var mongoose = require('mongoose');
 // var path = require('path');
 // var app = express();
 
 
 // // Mongojs configuration
-// var databaseUrl = "mongodb://heroku_jcjhxvx7:lqsfg22shtl09e8vbn1igrke2p@ds127878.mlab.com:27878/heroku_jcjhxvx7";
-// var collections = ["newsStories"];
+var databaseUrl = "mongodb://heroku_jcjhxvx7:lqsfg22shtl09e8vbn1igrke2p@ds127878.mlab.com:27878/heroku_jcjhxvx7";
+var collections = ["newsStories"];
 
 
 // // Hook our mongojs config to the db var
-// var db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
+
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
 
 
 
@@ -68,35 +70,39 @@
 // // ================================================
 
 // // Run request to grab the HTML from google news
-// request("https://news.google.com/", function(error, response, html) {
+/*******
 
-//   // Load the HTML into cheerio
-//   var $ = cheerio.load(html);
+request("https://news.google.com/", function(error, response, html) {
 
-//   // Make an empty array for saving our scraped info
-//   var result = [];
+  // Load the HTML into cheerio
+  var $ = cheerio.load(html);
 
-//   // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-//   $("span.titletext").each(function(i, element) {
+  // Make an empty array for saving our scraped info
+  var result = [];
 
-//     /* Cheerio's find method will "find" the first matching child element in a parent.
-//      *    We start at the current element, then "find" its first child a-tag.
-//      *    Then, we "find" the lone child img-tag in that a-tag.
-//      *    Then, .attr grabs the imgs src value.
-//      * So: <figure>  ->  <a>  ->  <img src="link">  ->  "link"  */
-//     var headline = $(element).text();//.find("a").attr("href")//.find("a")//.find("span").find("img")//.attr("src");//.attr("class").find("img").attr("src");
+  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
+  $("span.titletext").each(function(i, element) {
 
-//     // Push the image's URL (saved to the imgLink var) into the result array
-//     // result.push({ Link: headline });
-//     // db.newsStories.save({Headline: headline})
-//   });
+    // Cheerio's find method will "find" the first matching child element in a parent.
+    //    We start at the current element, then "find" its first child a-tag.
+    //    Then, we "find" the lone child img-tag in that a-tag.
+    //    Then, .attr grabs the imgs src value.
+    // So: <figure>  ->  <a>  ->  <img src="link">  ->  "link"  
+    var headline = $(element).text();//.find("a").attr("href")//.find("a")//.find("span").find("img")//.attr("src");//.attr("class").find("img").attr("src");
 
-//   // With each link scraped, log the result to the console
-//   // console.log(result);
+    // Push the image's URL (saved to the imgLink var) into the result array
+    // result.push({ Link: headline });
+    // db.newsStories.save({Headline: headline})
+  });
+
+  // With each link scraped, log the result to the console
+  // console.log(result);
   
-// });
+}); 
 
-// module.exports = db;
+******/
+
+module.exports = db;
 
 // app.listen(3001, function() {
 // 	console.log("App running on port: 3001!");
